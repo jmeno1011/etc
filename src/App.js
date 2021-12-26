f990f6de64dc0d58f1fe131783e96c4533035b49
meno1011 <39692905+meno1011@users.noreply.github.com>
2021-12-27 01:35:20 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import logo1 from "./meno-logo.png";
+import logo2 from "./meno_logo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <section style={{ display: "flex" }} className="header">
+        {/* <img src={logo1} style={{ height: "1rem" }} /> */}
+        <img src={logo2} />
+        <h1>blog</h1>
+      </section>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Food</a>
+            </li>
+            <li>
+              <a href="">Hobby</a>
+            </li>
+            <li>
+              <a href="">Game</a>
+            </li>
+            <li>
+              <a href="">Dream</a>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <div>
+        <h1>추가할내용</h1>
+      </div>
     </div>
   );
 }
